c9d51cc741b922fc9f968b6d15793b8b5ba480da
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-11-07 16:35:10 -0600

    ADD función signo

---
+++ RaicesFunciones/FuncionesAuxiliares.py
@@ -0,0 +1,11 @@
+def __Signo(valor:float) -> int:
+    """
+        Función auxiliar que devuelve 
+        el signo de una cantidad.
+
+        valor : float :: Valor a calcular 
+        su signo
+
+        Devuelve el singo de valor.
+    """
+    return 1 if 0 < valor else -1
