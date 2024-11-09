44b0ff4ddf741b8deb2c1564dc2d44ddd54b75b5
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-11-09 17:02:24 -0600

    ADD algoritmos de integración numérica al readme y __init__ del submodulo

--- DerivacionIntegracion/README.md
+++ DerivacionIntegracion/README.md
@@ -2,3 +2,4 @@
 
 Este submódulo se enceuntran implementado los algoritmos relacionados con la **Derivación e Integración Numérica**. Se encuentra: 
 * Algoritmo de diferenciación usando la definición de derivada evaluada en un punto y usando derivadas derivadas de la interpolación de Lagrange
+* Algoritmmos de integración usando las reglas trapezoidal y de Simpson para intervalos relativamente pequeños
--- DerivacionIntegracion/__init__.py
+++ DerivacionIntegracion/__init__.py
@@ -1,4 +1,6 @@
 from .DerivacionNumerica import DiferenciacionHaciaPaso , DiferenciaHaciaDelante , DiferenciaHaciaAtras , PrimeraDerivadaTresPuntos , SegundaDerivadaTresPuntos
+from .IntegracionNumerica import ReglaTrapezoidal , ReglaSimpson
 
 __all__ = ['DiferenciacionHaciaPaso' , 'DiferenciaHaciaDelante' , 'DiferenciaHaciaAtras',
-           'PrimeraDerivadaTresPuntos' , 'SegundaDerivadaTresPuntos']
+           'PrimeraDerivadaTresPuntos' , 'SegundaDerivadaTresPuntos',
+           'ReglaTrapezoidal' , 'ReglaSimpson']
