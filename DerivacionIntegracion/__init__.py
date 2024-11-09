424e7ee95abe04d38e3ba5a5656d2f14c955f56c
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-11-09 16:41:07 -0600

    ADD __init__ de Derivacción e Integración y submodulo al __init__ del proyecto

---
+++ DerivacionIntegracion/__init__.py
@@ -0,0 +1,3 @@
+from .DerivacionNumerica import DiferenciacionHaciaPaso , DiferenciaHaciaDelante , DiferenciaHaciaAtras
+
+__all__ = ['DiferenciacionHaciaPaso' , 'DiferenciaHaciaDelante' , 'DiferenciaHaciaAtras']
--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .SistemasLineales import *
 from .Interpolacion import *
 from .MinimosCuadrados import * 
 from .RaicesFunciones import *
+from .DerivacionIntegracion import *
