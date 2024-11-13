4e94dd036e7f442ee8e7697a7ca0c490775b66c0
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-11-13 10:05:05 -0600

    ADD método para integración aproximada dividiendo el intervalo en intervalos más pequeños

--- DerivacionIntegracion/IntegracionNumerica.py
+++ DerivacionIntegracion/IntegracionNumerica.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def ReglaTrapezoidal(Funcion,limiteInferiorIntegracion:float,limiteSuperiorIntegracion:float) -> float:
     """
         Función para integral de forma definida 
@@ -34,3 +36,27 @@ def ReglaSimpson(Funcion,limiteInferiorIntegracion:float,limiteSuperiorIntegraci
     diferenciaIntervalo = (limiteSuperiorIntegracion - limiteInferiorIntegracion)/2
     puntoMedio = (limiteInferiorIntegracion + limiteSuperiorIntegracion)/2
     return (diferenciaIntervalo/3) * (Funcion(limiteInferiorIntegracion) + 4*Funcion(puntoMedio) + Funcion(limiteSuperiorIntegracion))
+
+def IntegracionAproximada(Funcion,limiteInferiorIntegracion:float,limiteSuperiorIntegracion:float,divisiones:int=10) -> float:
+    """
+        Función que aproxima la integral 
+        definida de una función dado el 
+        intervalo que lo divide en intervalos 
+        más pequeños.
+
+        Funcion :: Función a integrar 
+        limiteInferiorIntegracion : float :: Límite 
+        inferior de integración
+        limiteSuperiorIntegracion : float :: Límite 
+        superior de integración
+        divisiones : int :: Cantidad de subintervalos 
+        que se usan para calcular la integral
+
+        Devuelve el valor aproximado de la integral 
+        de la función en el intervalo dado.
+    """
+    subintervalosDivisiones = np.linspace(limiteInferiorIntegracion,limiteSuperiorIntegracion,divisiones)
+    integralEvaluada = 0
+    for i_division in range(divisiones-1):
+        integralEvaluada += ReglaSimpson(Funcion,subintervalosDivisiones[i_division],subintervalosDivisiones[i_division+1])
+    return integralEvaluada
